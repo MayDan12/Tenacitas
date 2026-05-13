ff90edf99471fde60b6880a6e2c8af10ea843104
MayDan12 <oluwamayowadan@gmail.com>
2026-05-13 13:54:15 +0100

    Added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,11 +5,24 @@ import WhoIsItFor from "@/components/home/whoisthisfor";
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <WhoIsItFor />
-      <OurSolution />
-      <Faq />
-    </>
+    // <>
+    //   <Hero />
+    //   <WhoIsItFor />
+    //   <OurSolution />
+    //   <Faq />
+    // </>
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Site Under Maintenance</h1>
+      <p>We’ll be back shortly.</p>
+    </main>
   );
 }
